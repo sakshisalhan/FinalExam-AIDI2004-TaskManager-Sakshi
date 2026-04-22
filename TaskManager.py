0f206f7739002258dfe6496f718e07ee44aa3ea0
Sakshi <sakshi@example.com>
2026-04-22 14:03:03 -0400

    Added deleteTask method - closes #1 - Sakshi

--- TaskManager.py
+++ TaskManager.py
@@ -3,3 +3,9 @@
 class TaskManager:
     def addTask(self, title, priority):
         print(f"Task '{title}' added with priority {priority}")
+
+    def deleteTask(self, task_id):
+        """
+        Deletes a task based on task ID
+        """
+        print(f"Task with ID {task_id} deleted")
